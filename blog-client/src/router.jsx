3da76dadeb70c42edb476addd4c3f4292385dc02
Petra-78 <partos.petra.0305@gmail.com>
2026-01-08 21:00:36 +0100

    Add routes

---
+++ blog-client/src/router.jsx
@@ -0,0 +1,24 @@
+import Home from "./pages/home/Home";
+import App from "./App";
+import Error from "./components/Error/Error";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import AdminLogin from "./pages/login/AdminLogin";
+import Posts from "./components/Posts/Posts";
+
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/posts", element: <Posts /> },
+      { path: "/login", element: <Login /> },
+      { path: "/signup", element: <Signup /> },
+      { path: "/admin", element: <AdminLogin /> },
+    ],
+  },
+];
+
+export default routes;
