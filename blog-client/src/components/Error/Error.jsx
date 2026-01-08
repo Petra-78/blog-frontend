c5e61f18a89d8a4f534ba71a94d596b87a4bcc8b
Petra-78 <partos.petra.0305@gmail.com>
2026-01-08 18:50:47 +0100

    Add error page

---
+++ blog-client/src/components/Error/Error.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router";
+
+export default function Error() {
+  return (
+    <div className={styles.main}>
+      <h1>Uh oh...</h1>
+      <img src="" alt="sad face" />
+      <h2>Something must have went wrong :(</h2>
+      <Link to="/">
+        <button>Return to homepage</button>
+      </Link>
+    </div>
+  );
+}
