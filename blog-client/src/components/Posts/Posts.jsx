42e59343736292a86df98a0563ff0b204fb69e8d
Petra-78 <partos.petra.0305@gmail.com>
2026-01-09 16:36:43 +0100

    Add loading state + link to posts

--- blog-client/src/components/Posts/Posts.jsx
+++ blog-client/src/components/Posts/Posts.jsx
@@ -1,26 +1,42 @@
 import { useState, useEffect } from "react";
 import { getPosts } from "../../services/api";
+import { Link, useParams } from "react-router";
+import { Loading } from "../Loading/Loading";
 
 export default function Posts() {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
-      const postsData = await getPosts();
-      setPosts(postsData);
-    }
+      try {
+        const postsData = await getPosts();
+        setPosts(postsData);
 
+        console.log(posts);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    }
     fetchData();
   }, []);
+
   return (
     <div>
-      if(!posts){<h1>No posts yet</h1>}
-      {posts.map((post) => (
-        <div key={post.id}>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-        </div>
-      ))}
+      {!posts && <p>No posts yet.</p>}
+      {loading && <Loading />}
+      {posts &&
+        posts.length > 0 &&
+        posts.map((post) => (
+          <Link to={`/posts/${post.id}`}>
+            <div key={post.id}>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </Link>
+        ))}
     </div>
   );
 }
