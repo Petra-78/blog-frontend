79613f937c3fc0131c0a79d5e631e5b98cc2becf
Petra-78 <partos.petra.0305@gmail.com>
2026-01-09 18:10:50 +0100

    Import auth checker context

--- blog-client/src/components/Posts/Post.jsx
+++ blog-client/src/components/Posts/Post.jsx
@@ -1,13 +1,19 @@
-import { useState, useEffect } from "react";
-import { useParams } from "react-router";
+import { useState, useEffect, useContext } from "react";
+import { Link, useParams } from "react-router";
 import { getPost } from "../../services/api";
 import { Loading } from "../Loading/Loading";
+import Comments from "../Comments/Comments";
+import { CommentForm } from "../Comments/CommentForm";
+import { useAuth } from "../../context/authContext";
 
 export default function Post() {
+  debugger;
   const [post, setPost] = useState([]);
   const [loading, setLoading] = useState(true);
   const params = useParams();
   const id = params.id;
+
+  const { isAuthenticated } = useAuth();
 
   useEffect(() => {
     async function fetchData() {
@@ -31,6 +37,14 @@ export default function Post() {
         <div key={post.id}>
           <h2>{post.title}</h2>
           <p>{post.content}</p>
+          {isAuthenticated ? (
+            <CommentForm postId={post.id} />
+          ) : (
+            <Link to={"/login"}>
+              <button>Log in to leave a comment</button>
+            </Link>
+          )}
+          <Comments />
         </div>
       )}
       {loading && <Loading />}
