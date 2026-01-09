7ee9836a2158693072489cf1a641c709323c9bbd
Petra-78 <partos.petra.0305@gmail.com>
2026-01-09 18:16:14 +0100

    Add auth provider

--- blog-client/src/main.jsx
+++ blog-client/src/main.jsx
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import routes from "./router.jsx";
+import { AuthProvider } from "./context/authContext.jsx";
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>
 );
