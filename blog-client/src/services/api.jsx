fbe85df88164561426dae1913816dbdbc26e7f5f
Petra-78 <partos.petra.0305@gmail.com>
2026-01-09 16:37:17 +0100

    Add single post fetchimg function

--- blog-client/src/services/api.jsx
+++ blog-client/src/services/api.jsx
@@ -1,7 +1,17 @@
 export async function getPosts() {
+  debugger;
   const res = await fetch(
     "https://blog-api-production-323f.up.railway.app/posts"
   );
-  const data = await res.json;
+  const data = await res.json();
   return data.posts;
+}
+
+export async function getPost(id) {
+  debugger;
+  const res = await fetch(
+    `https://blog-api-production-323f.up.railway.app/posts/${id}`
+  );
+  const data = await res.json();
+  return data.post;
 }
