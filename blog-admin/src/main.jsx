3f743bff9ee6da3c7b5d0eff38f7b8b65feaeb2b
Petra-78 <partos.petra.0305@gmail.com>
2026-01-10 22:21:21 +0100

    Add router

--- blog-admin/src/main.jsx
+++ blog-admin/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { AuthProvider } from "./context/authContext.jsx";
+import routes from "./router.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(routes);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </StrictMode>
+);
