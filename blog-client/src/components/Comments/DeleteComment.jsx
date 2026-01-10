b2db1cfcb01663a5028129243f319568c6382846
Petra-78 <partos.petra.0305@gmail.com>
2026-01-10 20:15:07 +0100

    Add comment delete button

---
+++ blog-client/src/components/Comments/DeleteComment.jsx
@@ -0,0 +1,32 @@
+import { useAuth } from "../../context/authContext";
+
+export function DeleteComment({ commentId, onDeleted }) {
+  debugger;
+  const { token } = useAuth();
+
+  const handleDelete = async () => {
+    try {
+      const res = await fetch(
+        `https://blog-api-production-323f.up.railway.app/comments/${commentId}`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (!res.ok) {
+        throw new Error("Failed to delete comment");
+      }
+
+      if (onDeleted) {
+        onDeleted(commentId);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return <button onClick={handleDelete}>Delete</button>;
+}
