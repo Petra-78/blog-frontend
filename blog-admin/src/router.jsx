0c4396ce9495da59ba2c8dbce62d600d76f0709d
Petra-78 <partos.petra.0305@gmail.com>
2026-01-11 13:20:12 +0100

    Add post editor form

--- blog-admin/src/router.jsx
+++ blog-admin/src/router.jsx
@@ -1,4 +1,5 @@
 import PostList from "./components/PostList";
+import EditForm from "./components/PostForm";
 import App from "./App";
 
 const routes = [
@@ -6,7 +7,13 @@ const routes = [
     path: "/",
     element: <App />,
     // errorElement: <Error />,
-    children: [{ index: true, element: <PostList /> }],
+    children: [
+      { index: true, element: <PostList /> },
+      {
+        path: "/edit/:id",
+        element: <EditForm />,
+      },
+    ],
   },
 ];
 
