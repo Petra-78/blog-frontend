ef820fad4ff6b27d16a8c283e03799d3d180dfe2
Petra-78 <partos.petra.0305@gmail.com>
2026-01-12 22:55:58 +0100

    Add truncated html for content

--- blog-client/src/components/Posts/Posts.jsx
+++ blog-client/src/components/Posts/Posts.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { getPosts } from "../../services/api";
 import { Link } from "react-router";
 import { Loading } from "../Loading/Loading";
+import truncate from "truncate-html";
 
 export default function Posts() {
   const [posts, setPosts] = useState([]);
@@ -33,7 +34,11 @@ export default function Posts() {
           <Link to={`/posts/${post.id}`}>
             <div key={post.id}>
               <h2>{post.title}</h2>
-              <p>{post.content}</p>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: truncate(post.content, 100, { ellipsis: "..." }),
+                }}
+              />
             </div>
           </Link>
         ))}
