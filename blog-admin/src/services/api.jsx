a720332e881e45894bfdb208a50924106ed0a246
Petra-78 <partos.petra.0305@gmail.com>
2026-01-12 19:16:06 +0100

    Add create and delete functions

--- blog-admin/src/services/api.jsx
+++ blog-admin/src/services/api.jsx
@@ -15,6 +15,21 @@ export async function getPost(id) {
   return data.post;
 }
 
+export async function createPost(title, content, published, token) {
+  debugger;
+  const res = await fetch(
+    `https://blog-api-production-323f.up.railway.app/posts`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ title, content, published }),
+    }
+  );
+}
+
 export async function togglePostPublished(id, published, token) {
   const res = await fetch(
     `https://blog-api-production-323f.up.railway.app/posts/${id}`,
@@ -33,4 +48,25 @@ export async function togglePostPublished(id, published, token) {
   }
 
   return res.json();
+}
+
+export async function deletePost(id, token) {
+  debugger;
+  const res = await fetch(
+    `https://blog-api-production-323f.up.railway.app/posts/${id}`,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message || "Failed to delete post");
+  }
+
+  return data;
 }
