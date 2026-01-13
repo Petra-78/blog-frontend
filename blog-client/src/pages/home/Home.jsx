6e52522fe230ab9640888ee543ece2259e6e3278
Petra-78 <partos.petra.0305@gmail.com>
2026-01-13 18:23:25 +0100

    Add homepage styling

--- blog-client/src/pages/home/Home.jsx
+++ blog-client/src/pages/home/Home.jsx
@@ -2,25 +2,35 @@ import { useContext, useState } from "react";
 import codingImage from "../../assets/coding.jpg";
 import Posts from "../../components/Posts/Posts";
 import { useAuth } from "../../context/authContext";
+import styles from "./Home.module.css";
 
 export default function Home() {
   const { isAuthenticated } = useAuth();
 
   return (
-    <div>
-      <div>
-        <h1>Welcome to From Scratch!</h1>
-        <p>
-          This blog is a collection of thoughts, experiences, and experiments of
-          my journey. From web development projects to lessons learned along the
-          way, you can find anything. It’s a mix of code, curiosity, and
-          everyday reflections.
-        </p>
+    <div className={styles.wrapper}>
+      <div className={styles.intro}>
+        <div className={styles.text}>
+          <h1 className={styles.title}>Welcome to From Scratch!</h1>
+          <p className={styles.description}>
+            This blog is a collection of thoughts, experiences, and experiments
+            of my journey. From web development projects to lessons learned
+            along the way, you can find anything. It’s a mix of code, curiosity,
+            and everyday reflections.
+          </p>
+        </div>
+
+        <div className={styles.imageWrapper}>
+          <img
+            src={codingImage}
+            alt="Coding on a computer"
+            className={styles.image}
+          />
+        </div>
       </div>
-      <div>
-        <img src={codingImage} alt="Coding on a computer" />
-      </div>
-      <div>
+
+      <div className={styles.posts}>
+        <h2>Featured posts</h2>
         <Posts />
       </div>
     </div>
