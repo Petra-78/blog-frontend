653869ed6300486d244a1a8cbc4f00a1100d149c
Petra-78 <partos.petra.0305@gmail.com>
2026-01-13 17:08:57 +0100

    Add navbar styling

--- blog-client/src/components/Navbar/Navbar.jsx
+++ blog-client/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from "react-router";
 import { useAuth } from "../../context/authContext";
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   const { isAuthenticated, logout, user } = useAuth();
@@ -11,22 +12,30 @@ export default function Navbar() {
   };
 
   return (
-    <nav>
-      <div>
-        <Link to="/">Home</Link>
+    <nav className={styles.navbar}>
+      <div className={styles.left}>
+        <h1>From Scratch </h1>
       </div>
 
-      <div>
+      <div className={styles.right}>
+        <Link to="/" className={styles.link}>
+          Home
+        </Link>
         {!isAuthenticated ? (
           <>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign up</Link>
+            <Link to="/login" className={styles.link}>
+              Login
+            </Link>
+            <Link to="/signup" className={styles.link}>
+              Sign up
+            </Link>
           </>
         ) : (
           <>
-            {" "}
-            <h2>Welcome, {user.username}</h2>
-            <button onClick={handleLogout}>Logout</button>
+            <h2 className={styles.welcome}>Welcome, {user.username}</h2>
+            <button className={styles.logout} onClick={handleLogout}>
+              Logout
+            </button>
           </>
         )}
       </div>
